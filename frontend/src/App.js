4bc8fd49b04689186006c39e5c098af8bdefa707
Joseph D'ambra <josephdambra@Josephs-MacBook-Pro.local>
2021-01-14 16:07:02 +0000

    new select and table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Register from "./components/Register";
-import Login from './components/Login'
-import Quiz from './components/Quiz'
-import Users from "./components/Users";
+import Login from "./components/Login";
+import Quiz from "./components/Quiz";
 import Select from "./components/Select";
+import Results from "./components/Results";
+import Table from "./components/Table";
 import "./App.css";
 
 function App() {
@@ -15,8 +16,9 @@ function App() {
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/quiz" component={Quiz} />
-        <Route exact path="/users" component={Users} />
         <Route exact path="/select" component={Select} />
+        <Route exact path="/results" component={Results} />
+        <Route exact path="/table" component={Table} />
       </Switch>
     </BrowserRouter>
   );
---
+++ frontend/src/components/Table.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import "./table.css";
+
+class Table extends Component {
+  state = {
+    scores: [
+      { name: "John", score: 8, time: 120 },
+      { name: "Rodger", score: 10, time: 140 },
+      { name: "Lola", score: 5, time: 180 },
+      { name: "Mike", score: 5, time: 140 },
+      { name: "Chris", score: 10, time: 135 },
+    ],
+  };
+
+  componentDidMount() {
+    let data = this.state.scores;
+    let sorted = data.sort((a, b) => {
+      return b.score - a.score || a.time - b.time;
+    });
+
+    this.setState({ scores: sorted });
+  }
+  render() {
+    const { scores } = this.state;
+    return (
+      <div className="main">
+        <h1>Scoreboard</h1>
+        <div className="titles">
+          <h3>Username</h3>
+          <h3>Score</h3>
+          <h3>Time</h3>
+        </div>
+        {scores.map((person) => {
+          return (
+            <div className="table">
+              <p>{person.name}</p>
+              <p>{person.score}</p>
+              <p>{person.time}</p>
+            </div>
+          );
+        })}
+        <br />
+
+        <button>Try Again</button>
+
+        <button>Logout</button>
+      </div>
+    );
+  }
+}
+
+export default Table;
+
+// <p>
+//                 {person.name} - {person.score} - {person.time}
+//               </p>
