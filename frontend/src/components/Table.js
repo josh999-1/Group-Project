b43ba9125867ee3de78f1ed5fdee2892bdb225f7
Joseph D'ambra <josephdambra@Josephs-MacBook-Pro.local>
2021-01-20 14:34:17 +0000

    dont merge

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,7 +1,6 @@
 import React, { Component, useEffect, useState } from "react";
 import axios from 'axios'
 import "./table.css";
-<<<<<<< HEAD
 import Confetti from "./Confetti";
 
 class Table extends Component {
@@ -13,17 +12,7 @@ class Table extends Component {
       { name: "Mike", score: 5, time: 140 },
       { name: "Chris", score: 10, time: 135 },
     ],
-  };
-=======
-import makeConfetti from "./Confetti";
-import results from "./Results";
-import { useHistory } from "react-router-dom";
-
-const Table = () => {
-  const [scores, setScores] = useState([])
-  const [curScore, setcurScore] = useState({})
-  const [name, setName] = useState("")
->>>>>>> 71fdfca9c39e1619d27b81bf2c72a6e1848cdf7f
+  }
 
   const fetchData = async () => {
     const response = await axios.get('/table')
@@ -56,7 +45,6 @@ const Table = () => {
     return (
       <div className="mainTable">
         <h1 className="scoreboard">Scoreboard</h1>
-<<<<<<< HEAD
         <Confetti />
         <h3>Congratulations user, you got 5/10 correct in 1m 35s. </h3>
         <h3>Check your how you did on our scoreboard below</h3>
@@ -70,25 +58,9 @@ const Table = () => {
             <div className="table">
               <div className="name">
                 <p>{person.name}</p>
-=======
-        <p>Congratulations {name}, you got {curScore.score}/10 in {curScore.time}</p>
-          <div className="titles">
-            <h3 className="userName">Username</h3>
-            <h3 className="score">Score</h3>
-            <h3 className="time">Time</h3>
-          </div>
-          {scores.map((scores) => {
-            return (
-              <div className="table">
-                <div className="name">
-                  <p>{scores.userid.name}</p>
-                </div>
-                <p className="userScore">{scores.score}</p>
-                <p className="userTime">{scores.time}</p>
->>>>>>> 71fdfca9c39e1619d27b81bf2c72a6e1848cdf7f
               </div>
             );
-          })}
+          })
           <br />  
         
         <button onClick={handleClick} >Try Again</button>      
@@ -99,72 +71,6 @@ const Table = () => {
   return(
     <h1>{componentDid()}</h1>
   )
-}
+}  };
 
-<<<<<<< HEAD
 export default Table;
-=======
-export default Table
-
-
-// class Table extends Component {
-//   state = {
-//     scores: [
-//       { name: "John", score: 8, time: 120 },
-//       { name: "Rodger", score: 10, time: 140 },
-//       { name: "Lola", score: 5, time: 180 },
-//       { name: "Mike", score: 5, time: 140 },
-//       { name: "Chris", score: 10, time: 135 },
-//       { name: "Chris", score: 10, time: 135 },
-//       { name: "Chris", score: 10, time: 135 },
-//     ],
-//   };
-
-//   handleClick() {
-//     this.props.history.push('/select');
-//   }
-   
-
-// componentDidMount() {
-//   let data = this.state.scores;
-//   let sorted = data.sort((a, b) => {
-//     return b.score - a.score || a.time - b.time;
-//   });
-
-//     this.setState({ scores: sorted });
-//   }
-//   render() {
-//     const { scores } = this.state;
-//     return (
-//       <div className="main">
-//         <h1>Scoreboard</h1>
-//         <div className="titles">
-//           <h3>Username</h3>
-//           <h3>Score</h3>
-//           <h3>Time</h3>
-//         </div>
-//         {scores.map((person) => {
-//           return (
-//             <div className="table">
-//               <p>{person.name}</p>
-//               <p>{person.score}</p>
-//               <p>{person.time}</p>
-//             </div>
-//           );
-//         })}
-//         <br />
-
-//         <button onClick={() => this.handleClick()} >Try Again</button>
-
-//         <button>Logout</button>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Table;
-
-// <p>
-//                 {person.name} - {person.score} - {person.time}
-//               </p>
->>>>>>> 71fdfca9c39e1619d27b81bf2c72a6e1848cdf7f
